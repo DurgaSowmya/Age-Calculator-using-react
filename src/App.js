56b3269246fbc5384bcae7c1f491f8756c76cb94
DurgaSowmya <durgasowmyakollipara131203@gmail.com>
2023-10-13 21:28:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [dob, setDob] = useState("");
+  const [age, setAge] = useState("");
+
+  const calculateAge = () => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    const ageInMilliseconds = today - birthDate;
+    const ageInYears = Math.floor(
+      ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000)
+    );
+    setAge(ageInYears);
+  };
+
+  const handleInputChange = (e) => {
+    setDob(e.target.value);
+  };
+  var mydivstyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    textAlign: "center",
+  };
+  var mybuttonstyle = {
+    backgroundColor: "#007fff",
+    borderRadius: "10%",
+    color: "white",
+    border: "0px",
+    width: "120px",
+    height: "40px",
+  };
+  var myinputstyle = {
+    width: "350px",
+    height: "30px",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={mydivstyle} className="App">
+      <h1>Age Calculator</h1>
+      <br />
+      <h5>Enter your date of birth</h5>
+      <input
+        style={myinputstyle}
+        type="date"
+        id="dob"
+        value={dob}
+        onChange={handleInputChange}
+      />
+      <br />
+      <br />
+      <button style={mybuttonstyle} onClick={calculateAge}>
+        Calculate Age
+      </button>
+      {age && <h2>You are {age} years old</h2>}
     </div>
   );
 }
